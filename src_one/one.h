453b31722d7899fc12d24419d5706a0184f14294
Dan Engelbrecht <dan@engelbrecht.com>
2022-03-16 08:57:11 +0100

    Add some unformatted code

---
+++ src_one/one.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+struct One {
+    uint64_t m_Field1;
+    std::string m_Filed2;
+};
--- src_two/two.h
+++ src_two/two.h
@@ -1,1 +1,8 @@
+#pragma once
+
 #include "..\src_one\one.h"
+
+struct Two {
+    uint32_t m_Field1;
+    int m_Filed2;
+};
